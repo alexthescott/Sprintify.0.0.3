2db99ce42d8bf605cca8645fa14875f3d30ca02c
alexthescott <alexscott4@me.com>
2019-06-06 09:19:37 -0700

    Fixed PlaylistAdapter onClickListener title problem

--- auth-sample/src/main/java/com/spotify/sdk/android/authentication/sample/PlaylistAdapter.java
+++ auth-sample/src/main/java/com/spotify/sdk/android/authentication/sample/PlaylistAdapter.java
@@ -1,9 +1,9 @@
 package com.spotify.sdk.android.authentication.sample;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.media.Image;
 import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
@@ -15,7 +15,6 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class PlaylistAdapter extends RecyclerView.Adapter<PlaylistAdapter.PlaylistHolder> {
@@ -23,29 +22,68 @@ public class PlaylistAdapter extends RecyclerView.Adapter<PlaylistAdapter.Playli
     List playlistNames;
     List playlistCount;
     List playlistIDs;
+    List playlistHref;
+    String userId;
+    String accessToken;
     LayoutInflater mInflater;
+    Context ctx;
 
-    @NonNull
+    RecyclerView playlistRecycler;
+
+    private final View.OnClickListener playlistListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            int i = playlistRecycler.getChildLayoutPosition(v);
+
+            String currentHrefPlaylist = String.valueOf(playlistHref.get(i));
+            String currentNamePlaylist = String.valueOf(playlistNames.get(i));
+            String currentCountPlaylist = String.valueOf(playlistCount.get(i));
+
+            Log.d("currentNamePlaylist", currentNamePlaylist);
+            // Toast.makeText(ctx, String.valueOf(playlistNames.get(index)), Toast.LENGTH_SHORT).show();
+            Intent playlistIntent = new Intent(ctx, FilterActivity.class);
+            playlistIntent.putExtra("Token", accessToken);
+            playlistIntent.putExtra("Href", currentHrefPlaylist);
+            playlistIntent.putExtra("Name", currentNamePlaylist);
+            playlistIntent.putExtra("Count", currentCountPlaylist);
+            playlistIntent.putExtra("userID", userId);
+            ctx.startActivity(playlistIntent);
+            }
+    };
+
+    @Override
+    public void onAttachedToRecyclerView(@NonNull RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+        this.playlistRecycler = recyclerView;
+    }
+
     @Override
     public PlaylistHolder onCreateViewHolder(ViewGroup parent, int i) {
         View view = mInflater.inflate(R.layout.itemplaylist, parent, false);
+        view.setOnClickListener(playlistListener);
         return new PlaylistHolder(view);
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
-    public void onBindViewHolder(@NonNull PlaylistAdapter.PlaylistHolder playlistHolder, int i) {
-        playlistHolder.playlistCount.setText(playlistCount.get(i).toString());
-        playlistHolder.playlistTitle.setText(playlistNames.get(i).toString());
+    public void onBindViewHolder(@NonNull PlaylistHolder playlistHolder, int i) {
+        String currentNamePlaylist = String.valueOf(playlistNames.get(i));
+        String currentCountPlaylist = String.valueOf(playlistCount.get(i));
+
+        playlistHolder.playlistCount.setText(currentCountPlaylist);
+        playlistHolder.playlistTitle.setText(currentNamePlaylist);
         byte[] imageByte = playlistDB.getIMGByte((String) playlistIDs.get(i));
         Bitmap image = BitmapFactory.decodeByteArray(imageByte, 0, imageByte.length);
         playlistHolder.playlistImage.setImageBitmap(image);
     }
 
-    public PlaylistAdapter(Context ctx, List newPlaylistNames, List newPlaylistCount, List newPlaylistIDs){
+    public PlaylistAdapter(Context ctx, List newPlaylistNames, List newPlaylistCount, List newPlaylistIDs, List newPlaylistHref, String newUserID, String newAccessToken){
         Log.d("TRACKADAPTER", "Created Instance");
         mInflater = LayoutInflater.from(ctx);
         playlistDB = new PlaylistDB(ctx, "PL_IMAGE_DATABASE", null, 1);
+        this.ctx = ctx;
+        this.userId = newUserID;
+        this.accessToken = newAccessToken;
+        this.playlistHref = newPlaylistHref;
         this.playlistNames = newPlaylistNames;
         this.playlistCount = newPlaylistCount;
         this.playlistIDs = newPlaylistIDs;
